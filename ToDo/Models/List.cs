6ee1f4de72848b8399d48af16db512d095119d8c
JKSayers <jsayers1980@gmail.com>
2017-02-08 11:17:24 -0500

    first commit

---
+++ ToDo/Models/List.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ToDo.Models
+{
+    public class List
+    {
+        [Key]
+
+        public int ListID;
+        public string Task;
+
+
+
+
+        [ForeignKey]
+
+        public string Title;
+
+    }
+}
